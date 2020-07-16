ea8e605696033ef9b0706a37f4b4e40172e0176a
Mish_Sanek <kid02@list.ru>
2020-07-16 23:11:50 +0500

    Ничего не поменял. Скорость рендеринга нормальная, стейта у нас нет,т.к все в сторе. Но завел хок

---
+++ src/hocs/with-active-flag.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const withActiveFlag = (Component) => (props) => {
+  const [isActive, setActive] = React.useState(false);
+
+  const handleActiveChange = () => setActive(!isActive);
+
+  return (
+    <Component
+      {...props}
+      isActive={isActive}
+      onActiveChange={handleActiveChange}
+    />
+  );
+};
